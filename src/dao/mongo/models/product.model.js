11073049316502ffb9e332c34a0d606d58d6ef10
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-05-20 10:28:57 -0300

    refactorizar mongo models

--- src/dao/mongo/models/product.model.js
+++ src/dao/mongo/models/product.model.js
@@ -4,10 +4,10 @@ import mongoosePaginate from 'mongoose-paginate-v2';
 const productsCollection = 'products';
 
 const productSchema = new Schema({
-    title: { type: String, index: true },
+    title: { type: String, required: true, index: true },
     description: { type: String },
-    code: { type: String, unique: true, index: true },
-    price: { type: Number },
+    code: { type: String, required: true, unique: true, index: true },
+    price: { type: Number, required: true },
     status: { type: Boolean },
     stock: { type: Number },
     category: { type: String, index: true },
