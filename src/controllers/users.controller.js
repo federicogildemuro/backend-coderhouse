a981e741f0c396cbd8b8cc57bc3e80398b49ef28
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-06-01 09:30:36 -0300

    agregar users controller

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,45 @@
+import UsersServices from '../services/users.services.js';
+
+export default class UsersController {
+    static async getUsers(req, res) {
+        try {
+        } catch (error) {
+        }
+    }
+
+    static async getUserById(req, res) {
+        try {
+        } catch (error) {
+        }
+    }
+
+    static async createUser(req, res) {
+        try {
+        } catch (error) {
+        }
+    }
+
+    static async uploadUserDocument(req, res) {
+        try {
+        } catch (error) {
+        }
+    }
+
+    static async updateUser(req, res) {
+        try {
+        } catch (error) {
+        }
+    }
+
+    static async deleteUsers(req, res) {
+        try {
+        } catch (error) {
+        }
+    }
+
+    static async deleteUser(req, res) {
+        try {
+        } catch (error) {
+        }
+    }
+}
