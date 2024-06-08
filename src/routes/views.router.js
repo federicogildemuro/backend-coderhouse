94e2f6076f90c0ee1530b25e686670467461ece8
Federico Gil de Muro <federicogildemuro@gmail.com>
2024-06-08 07:27:35 -0300

    corregir error en policies

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -26,17 +26,17 @@ export default class ViewsRouter extends CustomRouter {
 
         this.get('/reset-password', ['PUBLIC'], ViewsController.getInstance().renderResetPassword);
 
-        this.get('/products', ['USER', 'PREMIUM'], ViewsController.getInstance().renderProducts);
+        this.get('/products', ['USER'], ViewsController.getInstance().renderProducts);
 
-        this.get('/product/:pid', ['USER', 'PREMIUM'], ViewsController.getInstance().renderProduct);
+        this.get('/product/:pid', ['USER'], ViewsController.getInstance().renderProduct);
 
-        this.get('/cart/:cid', ['USER', 'PREMIUM'], ViewsController.getInstance().renderCart);
+        this.get('/cart/:cid', ['USER'], ViewsController.getInstance().renderCart);
 
-        this.get('/profile', ['USER', 'PREMIUM'], ViewsController.getInstance().renderProfile);
+        this.get('/profile', ['USER'], ViewsController.getInstance().renderProfile);
 
-        this.get('/upload-documents', ['USER', 'PREMIUM'], ViewsController.getInstance().renderUploadDocuments);
+        this.get('/upload-documents', ['USER'], ViewsController.getInstance().renderUploadDocuments);
 
-        this.get('/chat', ['USER', 'PREMIUM'], ViewsController.getInstance().renderChat);
+        this.get('/chat', ['USER'], ViewsController.getInstance().renderChat);
 
         this.get('/premium/products', ['PREMIUM'], ViewsController.getInstance().renderPremiumProducts);
 
